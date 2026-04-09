25fa1a90664b4754fe7998c116f28a57ec922e3e
supermanhenry <145008344+priyansh-narang2308@users.noreply.github.com>
2026-04-09 19:23:47 +0530

    feat: The Task model added in mongodb

---
+++ backend/models/Task.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      trim: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      transform: (doc, ret) => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  },
+);
+
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;
