2a93961c116fc11ba24e72b27b6e4c3accce0c11
supermanhenry <145008344+priyansh-narang2308@users.noreply.github.com>
2026-04-09 19:25:38 +0530

    feat: Add task routes for CRUD operations

---
+++ backend/routes/tasks.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  getAllTasks,
+  createTask,
+  updateTask,
+  deleteTask,
+} from "../controllers/tasksController.js";
+
+const router = express.Router();
+
+router.get("/", getAllTasks);
+router.post("/", createTask);
+router.patch("/:id", updateTask);
+router.delete("/:id", deleteTask);
+
+export default router;
